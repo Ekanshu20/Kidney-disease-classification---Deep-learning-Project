26b2234c9fe1616903d2bd8c6cea39bae7ae38ad
Ekanshu <ekanshu20agrawal@gmail.com>
2024-02-18 15:18:14 +0530

    utils added

--- template.py
+++ template.py
@@ -21,7 +21,9 @@ list_of_files= [".github/workflows/.gitkeep",
                 "requirements.txt",
                 "setup.py",
                 "research/trials.ipynb",
-                "templates/index.html"
+                "templates/index.html",
+                "main.py"
+                
                      
       ]
 
